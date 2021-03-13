47499659ec1cffd0e7ab109e5346385d5de1e2b3
Ryli <rylidavis94@gmail.com>
2021-03-13 11:36:53 -0800

    Week 3 Workshop

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -10,7 +10,7 @@ import Contact from "./ContactComponent";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
-
+import About from "./AboutComponent";
 
 class Main extends Component {
 	constructor(props) {
@@ -24,53 +24,53 @@ class Main extends Component {
 	}
 
 	render() {
-		
-        const HomePage = () => {
-					return (
-						<Home
-							campsite={
-								this.state.campsites.filter((campsite) => campsite.featured)[0]
-							}
-							promotion={
-								this.state.promotions.filter(
-									(promotion) => promotion.featured
-								)[0]
-							}
-							partner={
-								this.state.partners.filter((partner) => partner.featured)[0]
-							}
-						/>
-					);
-				}
+		const HomePage = () => {
+			return (
+				<Home
+					campsite={
+						this.state.campsites.filter((campsite) => campsite.featured)[0]
+					}
+					promotion={
+						this.state.promotions.filter((promotion) => promotion.featured)[0]
+					}
+					partner={this.state.partners.filter((partner) => partner.featured)[0]}
+				/>
+			);
+		};
 
-
- const CampsiteWithId = ({ match }) => {
-		return (
-			<CampsiteInfo
-				campsite={
-					this.state.campsites.filter(
-						(campsite) => campsite.id === +match.params.campsiteId
-					)[0]
-				}
-				comments={this.state.comments.filter(
-					(comment) => comment.campsiteId === +match.params.campsiteId
-				)}
-			/>
-		);
- };    
+		const CampsiteWithId = ({ match }) => {
+			return (
+				<CampsiteInfo
+					campsite={
+						this.state.campsites.filter(
+							(campsite) => campsite.id === +match.params.campsiteId
+						)[0]
+					}
+					comments={this.state.comments.filter(
+						(comment) => comment.campsiteId === +match.params.campsiteId
+					)}
+				/>
+			);
+		};
 
 		return (
 			<div>
 				<Header />
 				<Switch>
-					<Route exact path="/contactus" component={Contact} />
 					<Route path="/home" component={HomePage} />
-					<Route path="/directory/:campsiteId" component={CampsiteWithId} />
 					<Route
 						exact
 						path="/directory"
 						render={() => <Directory campsites={this.state.campsites} />}
 					/>
+					<Route
+						exact
+						path="/aboutus"
+						render={() => <About partners={this.state.partners} />}
+					/>
+					<Route path="/directory/:campsiteId" component={CampsiteWithId} />
+					<Route exact path="/contactus" component={Contact} />
+
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
