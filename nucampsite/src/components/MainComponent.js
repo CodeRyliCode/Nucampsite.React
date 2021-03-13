f038f1b3e229d73f2c48e6cad19e72e776730a7e
Ryli <rylidavis94@gmail.com>
2021-03-13 00:11:28 -0800

    React Router Parameters

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -41,14 +41,31 @@ class Main extends Component {
 							}
 						/>
 					);
-				};
-                
+				}
+
+
+ const CampsiteWithId = ({ match }) => {
+		return (
+			<CampsiteInfo
+				campsite={
+					this.state.campsites.filter(
+						(campsite) => campsite.id === +match.params.campsiteId
+					)[0]
+				}
+				comments={this.state.comments.filter(
+					(comment) => comment.campsiteId === +match.params.campsiteId
+				)}
+			/>
+		);
+ };    
+
 		return (
 			<div>
 				<Header />
 				<Switch>
 					<Route exact path="/contactus" component={Contact} />
 					<Route path="/home" component={HomePage} />
+					<Route path="/directory/:campsiteId" component={CampsiteWithId} />
 					<Route
 						exact
 						path="/directory"
